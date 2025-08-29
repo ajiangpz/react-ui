6bb6d79ddbe48e32b47cdf2fa1a7ff28ba6b77a0
jiangpengzhen <jiangpengzhen@tenda.cn>
2025-08-29 15:17:30 +0800

    feat: 更新storybook

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -1,34 +1,35 @@
-import { Meta, StoryObj } from "@storybook/react";
-import { Input } from "./Input";
+import { Meta, StoryObj } from '@storybook/react';
+import Input from './index';
+import '@/components/style/index.js';
 
 const meta: Meta<typeof Input> = {
-  title: "Components/Input",
+  title: 'Components/Input',
   component: Input,
-  tags: ["autodocs"],
+  tags: ['autodocs'],
   argTypes: {
     type: {
-      control: "select",
+      control: 'select',
       options: [
-        "text",
-        "file",
-        "password",
-        "number",
-        "tel",
-        "url",
-        "search",
-        "date",
-        "time",
-        "datetime-local",
-        "month",
-        "week"
+        'text',
+        'file',
+        'password',
+        'number',
+        'tel',
+        'url',
+        'search',
+        'date',
+        'time',
+        'datetime-local',
+        'month',
+        'week',
       ],
-      defaultValue: "text"
+      defaultValue: 'text',
     },
     disabled: {
-      control: "boolean",
-      defaultValue: false
-    }
-  }
+      control: 'boolean',
+      defaultValue: false,
+    },
+  },
 };
 
 export default meta;
@@ -37,9 +38,9 @@ type Story = StoryObj<typeof Input>;
 
 export const Default: Story = {
   args: {
-    type: "text",
-    placeholder: "请输入内容",
+    type: 'text',
+    placeholder: '请输入内容',
     disabled: false,
-    name: "input",
-  }
+    name: 'input',
+  },
 };
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,21 +1,408 @@
-import * as React from "react";
+import React, {
+  useState,
+  useRef,
+  useEffect,
+  useImperativeHandle,
+  forwardRef,
+} from 'react';
+import classNames from 'classnames';
+import { Eye, EyeOff, XCircle } from 'lucide-react';
+import { TdInputProps } from './type';
+import useLayoutEffect from '@/hooks/useLayoutEffect';
+import useControlled from '@/hooks/useControlled';
+import { inputDefaultProps } from './defaultProps';
+import parseTNode from '@/lib/utils';
+import useDefaultProps from '@/hooks/useDefaultProps';
+import { StyledProps, TNode, TElement } from '../common';
+import { isFunction } from 'lodash-es';
 
-import { cn } from "@/lib/utils";
-
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
-  return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "file:text-foreground text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:select-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-danger/20 dark:aria-invalid:ring-danger/40 aria-invalid:border-danger",
-        className
-      )}
-      {...props}
-    />
-  );
+export interface InputProps extends TdInputProps, StyledProps {
+  showInput?: boolean; // 控制透传readonly同时是否展示input 默认保留 因为正常Input需要撑开宽度
+  keepWrapperWidth?: boolean; // 控制透传autoWidth之后是否容器宽度也自适应 多选等组件需要用到自适应但也需要保留宽度
 }
 
-export { Input };
+export interface InputRef extends React.RefObject<unknown> {
+  currentElement: HTMLDivElement;
+  inputElement: HTMLInputElement;
+  focus: () => void;
+  blur: () => void;
+  select: () => void;
+}
+
+type InputContextTrigger = 'input' | 'clear' | 'initial';
+
+const renderIcon = (
+  classPrefix: string,
+  type: 'prefix' | 'suffix',
+  icon: TNode | TElement,
+) => {
+  const result = parseTNode(icon);
+
+  const iconClassName = icon ? `t-input__${type}-icon` : '';
+
+  return result ? (
+    <span className={`t-input__${type} ${iconClassName}`}>{result}</span>
+  ) : null;
+};
+
+const Input = forwardRef<HTMLInputElement, InputProps>((originalProps, ref) => {
+  const props = useDefaultProps<InputProps>(originalProps, inputDefaultProps);
+  const {
+    type,
+    autoWidth,
+    borderless,
+    placeholder = '请输入内容',
+    disabled,
+    status,
+    size,
+    className,
+    inputClass,
+    style,
+    prefixIcon,
+    suffixIcon,
+    clearable,
+    tips,
+    align,
+    maxlength,
+    maxcharacter,
+    showClearIconOnEmpty,
+    autofocus,
+    autocomplete,
+    readonly,
+    label,
+    suffix,
+    showInput = true,
+    keepWrapperWidth,
+    showLimitNumber,
+    allowInput,
+    allowInputOverMax,
+    name,
+    format,
+    onClick,
+    onClear,
+    onEnter,
+    onKeydown,
+    onKeyup,
+    onKeypress,
+    onFocus,
+    onBlur,
+    onPaste,
+    onMouseenter,
+    onMouseleave,
+    onWheel,
+    onCompositionstart,
+    onCompositionend,
+    onValidate,
+    onChange: onChangeFromProps,
+    ...restProps
+  } = props;
+  const composingRef = useRef(false);
+  const [value, onChange] = useControlled(props, 'value', onChangeFromProps);
+  const inputRef: React.RefObject<HTMLInputElement | null> = useRef(null);
+  const inputPreRef: React.RefObject<HTMLInputElement | null> = useRef(null);
+  const wrapperRef: React.RefObject<HTMLDivElement | null> = useRef(null);
+
+  const [isHover, toggleIsHover] = useState(false);
+  const [isFocused, toggleIsFocused] = useState(false);
+  const [renderType, setRenderType] = useState(type);
+
+  const [composingValue, setComposingValue] = useState<string>('');
+  const isInnerInputReadonly = readonly || !allowInput;
+  const isValueEnabled = value && !disabled;
+  const isShowClearIcon =
+    ((clearable && isValueEnabled) || showClearIconOnEmpty) && isHover;
+  let suffixIconNew = suffixIcon;
+  if (isShowClearIcon)
+    suffixIconNew = (
+      <XCircle
+        className={`t-input__suffix-clear`}
+        onMouseDown={handleMouseDown}
+        onClick={handleClear}
+      />
+    );
+
+  if (type === 'password' && typeof suffixIcon === 'undefined') {
+    if (renderType === 'password') {
+      suffixIconNew = (
+        <EyeOff
+          className={`t-input__suffix-clear`}
+          onClick={togglePasswordVisible}
+        />
+      );
+    } else if (renderType === 'text') {
+      suffixIconNew = (
+        <Eye
+          className={`t-input__suffix-clear`}
+          onClick={togglePasswordVisible}
+        />
+      );
+    }
+  }
+  const prefixIconContent = renderIcon('t', 'prefix', parseTNode(prefixIcon));
+  const suffixIconContent = renderIcon(
+    't',
+    'suffix',
+    parseTNode(suffixIconNew),
+  );
+  const labelContent = isFunction(label) ? label() : label;
+  const suffixContent = isFunction(suffix) ? suffix() : suffix;
+
+  const updateInputWidth = () => {
+    if (!autoWidth || !inputRef.current) return;
+    const { offsetWidth } = inputPreRef.current as HTMLInputElement;
+    const { width } = inputPreRef.current?.getBoundingClientRect() as DOMRect;
+    // 异步渲染场景下 getBoundingClientRect 宽度为 0，需要使用 offsetWidth
+    const calcWidth = width < offsetWidth ? offsetWidth + 1 : width;
+    inputRef.current.style.width = `${calcWidth}px`;
+  };
+
+  useLayoutEffect(() => {
+    // 推迟到下一帧处理防止异步渲染 input 场景宽度计算为 0
+    requestAnimationFrame(() => {
+      updateInputWidth();
+    });
+    // eslint-disable-next-line
+  }, [autoWidth, value, placeholder, inputRef, composingValue]);
+
+  // 当元素默认为 display: none 状态，无法提前准确计算宽度，因此需要监听元素宽度变化。比如：Tabs 场景切换。
+  useEffect(() => {
+    let resizeObserver: ResizeObserver = null as any;
+    // IE 11 以下使用设置 minWidth 兼容；IE 11 以上使用 ResizeObserver
+    if (typeof window.ResizeObserver === 'undefined' || !inputRef.current)
+      return;
+    resizeObserver = new window.ResizeObserver(() => {
+      updateInputWidth();
+    });
+    resizeObserver.observe(inputRef.current);
+    return () => {
+      // resizeObserver.unobserve?.(inputRef.current);
+      resizeObserver.disconnect?.();
+      resizeObserver = null as any;
+    };
+    // eslint-disable-next-line
+  }, [inputRef]);
+
+  useEffect(() => {
+    setRenderType(type);
+  }, [type]);
+
+  const innerValue = composingRef.current ? composingValue : (value ?? '');
+  const formatDisplayValue =
+    format && !isFocused ? format(innerValue) : innerValue;
+
+  const renderInput = (
+    <input
+      ref={inputRef}
+      placeholder={placeholder}
+      type={renderType}
+      className={classNames(`t-input__inner`, {
+        [`t-input--soft-hidden`]: !showInput,
+      })}
+      value={formatDisplayValue}
+      readOnly={isInnerInputReadonly}
+      disabled={disabled}
+      autoComplete={autocomplete}
+      autoFocus={autofocus}
+      onChange={handleChange}
+      onKeyDown={handleKeyDown}
+      onKeyUp={handleKeyUp}
+      onKeyPress={handleKeyPress}
+      onCompositionStart={handleCompositionStart}
+      onCompositionEnd={handleCompositionEnd}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+      onPaste={handlePaste}
+      name={name}
+    />
+  );
+  const renderInputNode = (
+    <div
+      className={classNames(inputClass, `t-input`, {
+        [`t-is-readonly`]: readonly,
+        [`t-is-disabled`]: disabled,
+        [`t-is-focused`]: isFocused,
+        [`t-size-s`]: size === 'small',
+        [`t-size-l`]: size === 'large',
+        [`t-align-${align}`]: align,
+        [`t-input--prefix`]: prefixIcon || labelContent,
+        [`t-input--suffix`]: suffixIconContent || suffixContent,
+        [`t-input--borderless`]: borderless,
+        [`t-input--focused`]: isFocused,
+      })}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onWheel={(e) => onWheel?.({ e })}
+      onClick={(e) => {
+        inputRef.current?.focus();
+        onClick?.({ e });
+      }}
+    >
+      {prefixIconContent}
+      {labelContent ? (
+        <div className={`t-input__prefix`}>{labelContent}</div>
+      ) : null}
+      {renderInput}
+      {autoWidth && (
+        <span ref={inputPreRef} className={`t-input__input-pre`}>
+          {innerValue || placeholder}
+        </span>
+      )}
+      {suffixContent ? (
+        <div className={`t-input__suffix`}>{suffixContent}</div>
+      ) : null}
+      {suffixIconContent}
+    </div>
+  );
+  useLayoutEffect(() => {
+    // 推迟到下一帧处理防止异步渲染 input 场景宽度计算为 0
+    requestAnimationFrame(() => {
+      updateInputWidth();
+    });
+    // eslint-disable-next-line
+  }, [autoWidth, value, placeholder, inputRef, composingValue]);
+
+  function togglePasswordVisible() {
+    if (disabled) return;
+    // 保存光标位置
+    const inputEl = inputRef.current;
+    const cursorPosition = inputRef.current?.selectionStart || 0;
+
+    const toggleType = renderType === 'password' ? 'text' : 'password';
+    setRenderType(toggleType);
+
+    requestAnimationFrame(() => {
+      inputEl?.setSelectionRange(cursorPosition, cursorPosition);
+    });
+  }
+
+  function handleChange(
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.CompositionEvent<HTMLInputElement>,
+    trigger: InputContextTrigger = 'input',
+  ) {
+    let { value: newStr } = e.currentTarget;
+    if (composingRef.current) {
+      setComposingValue(newStr);
+    } else {
+      // 完成中文输入时同步一次 composingValue
+      setComposingValue(newStr);
+      onChange(newStr, { e, trigger });
+    }
+  }
+
+  // 添加MouseDown阻止冒泡，防止點擊Clear value會導致彈窗閃爍一下
+  // https://github.com/Tencent/tdesign-react/issues/2320
+  function handleMouseDown(
+    e: React.MouseEvent<SVGSVGElement, globalThis.MouseEvent>,
+  ) {
+    e.stopPropagation();
+    // 兼容React16
+    e.nativeEvent.stopImmediatePropagation();
+  }
+
+  function handleClear(e: React.MouseEvent<SVGSVGElement>) {
+    onChange?.('', { e, trigger: 'clear' });
+    onClear?.({ e });
+  }
+
+  function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    const {
+      key,
+      currentTarget: { value },
+    } = e;
+    key === 'Enter' && onEnter?.(value, { e });
+    onKeydown?.(value, { e });
+  }
+
+  function handleKeyUp(e: React.KeyboardEvent<HTMLInputElement>) {
+    const {
+      currentTarget: { value },
+    } = e;
+    onKeyup?.(value, { e });
+  }
+
+  function handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
+    const {
+      currentTarget: { value },
+    } = e;
+    onKeypress?.(value, { e });
+  }
+
+  function handleCompositionStart(e: React.CompositionEvent<HTMLInputElement>) {
+    composingRef.current = true;
+    const {
+      currentTarget: { value },
+    } = e;
+    onCompositionstart?.(value, { e });
+  }
+
+  function handleCompositionEnd(e: React.CompositionEvent<HTMLInputElement>) {
+    const {
+      currentTarget: { value },
+    } = e;
+    if (composingRef.current) {
+      composingRef.current = false;
+      handleChange(e);
+    }
+    onCompositionend?.(value, { e });
+  }
+
+  function handleFocus(e: React.FocusEvent<HTMLInputElement>) {
+    if (isInnerInputReadonly) return;
+    const {
+      currentTarget: { value },
+    } = e;
+    onFocus?.(value, { e });
+    toggleIsFocused(true);
+  }
+
+  function handleBlur(e: React.FocusEvent<HTMLInputElement>) {
+    if (isInnerInputReadonly) return;
+    const {
+      currentTarget: { value },
+    } = e;
+    onBlur?.(value, { e });
+    toggleIsFocused(false);
+  }
+
+  function handlePaste(e: React.ClipboardEvent<HTMLInputElement>) {
+    const clipData = e.clipboardData;
+    const pasteValue = clipData?.getData('text/plain');
+    onPaste?.({ e, pasteValue });
+  }
+
+  function handleMouseEnter(e: React.MouseEvent<HTMLDivElement>) {
+    !readonly && toggleIsHover(true);
+    onMouseenter?.({ e });
+  }
+
+  function handleMouseLeave(e: React.MouseEvent<HTMLDivElement>) {
+    !readonly && toggleIsHover(false);
+    onMouseleave?.({ e });
+  }
+
+  useImperativeHandle(ref as InputRef, () => ({
+    currentElement: wrapperRef.current,
+    inputElement: inputRef.current,
+    focus: () => inputRef.current?.focus(),
+    blur: () => inputRef.current?.blur(),
+    select: () => inputRef.current?.select(),
+  }));
+
+  return (
+    <div
+      ref={wrapperRef}
+      style={style}
+      className={classNames(`t-input__wrap`, className, {
+        [`t-input--auto-width`]: autoWidth && !keepWrapperWidth,
+      })}
+      {...restProps}
+    >
+      {renderInputNode}
+      {tips && <div className={classNames(`t-input__tips`)}>{tips}</div>}
+    </div>
+  );
+});
+Input.displayName = 'Input';
+export default Input;
